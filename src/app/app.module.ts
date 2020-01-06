71a11c64975ca396452d1fce0fb08b2d9f570db5
AmbarDudhane <ambarsd12345@hotmail.com>
2020-01-05 20:23:23 -0600

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from './material.module';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeeService } from './employee.service';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { RegisterSuccessComponent } from './register-success/register-success.component';
+import { UpdateSuccessComponent } from './update-success/update-success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    RegisterComponent,
+    AboutUsComponent,
+    RegisterSuccessComponent,
+    UpdateSuccessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
