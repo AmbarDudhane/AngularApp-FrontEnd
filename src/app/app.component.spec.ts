05b1c652f2a8b433f66705bcc5a38c107fa0a92d
AmbarDudhane <ambarsd12345@hotmail.com>
2020-01-05 20:23:31 -0600

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('finalapp app is running!');
+    expect(compiled.querySelector('.content mat-error').textContent).toContain('finalapp app is running!');
   });
 });
